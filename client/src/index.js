fa8e1201a6f0c9096f4af8bc4c9cc538f06c529c
trilambda122 <trilambda122>
2021-01-09 16:32:14 -0600

    changed homepage

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./App";
+import Home from "./components/Home";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
 
 serviceWorker.register();
